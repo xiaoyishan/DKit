421bd859ddad5647ef1173a7ff749dfd42c8fa34
Ys <ys@ysdemac-mini.local>
2017-07-20 23:14:16 +0800

    add file

---
+++ DKit/简单示例/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  DKit
+//
+//  Created by Sundear on 2017/4/20.
+//  Copyright © 2017年 xiexin. All rights reserved.
+//
+#import "Dkit.h"
+#import "UserView.h"
+#import "UserModel.h"
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    UserView *CustomView;
+}
+
+@property (weak, nonatomic) IBOutlet UserView *myView;
+
+
+@end
+
